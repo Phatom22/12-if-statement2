dc666f8ddef5418695f201fb9bceb94da4579890
nomvula <nzshabangu95@gmail.com>
2023-09-12 13:31:37 +0200

    Add project files.

---
+++ 12 if statement2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_if_statement2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number 1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter number 2");
+           int  num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(GetMax(num1,num2));
+
+            //freeze console
+            Console.ReadLine();
+
+        }
+        static int GetMax( int num1, int num2)
+        {
+
+            
+            if (num1 > num2)
+            {
+                Console.Write("the max is ");
+                return num1;
+            }
+            else {
+                Console.Write("the max is ");
+                return num2;
+            }
+        }
+    }
+}
